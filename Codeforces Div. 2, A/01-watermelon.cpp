8c113319c1d8bbf7a1eac351ae97b9a00c0b430a
Rr1901 <60656712+Rr1901@users.noreply.github.com>
2020-10-01 11:16:56 +0530

    Update 01-watermelon.cpp

--- Codeforces Div. 2, A/01-watermelon.cpp
+++ Codeforces Div. 2, A/01-watermelon.cpp
@@ -3,30 +3,25 @@
  * DATE : 04-May-20
 */
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map>
-#include <cstring>
-#include <cstdio>
-#include <climits>
-
+#include<bits/stdc++.h>
+using namespace std;
 typedef long long int lli;
 typedef unsigned int ui;
 typedef unsigned long long ull;
 
+
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define mod 1e9+7
 #define PI 3.14159265359
 #define ss second
 #define ff first
 #define pb push_back
-#define vi std::vector <int>
+#define vi vector <int>
 #define endl "\n"
 
 int main(){
     
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0);
+    IOS    
 
     #ifndef ONLINE_JUDGE
         freopen("input.in", "r", stdin);
@@ -35,12 +30,12 @@ int main(){
 
     int n;
 
-    std::cin >> n;
+    cin >> n;
 
     if (n == 2 || n % 2 == 1)
-        std::cout << "NO" << endl;
+        cout << "NO" << endl;
     else    
-        std::cout << "YES" << endl;
+        cout << "YES" << endl;
     return 0;
 }
 
